6145dc72b52081d05a619d68b461fdbdb0111465
Isaac Park <isaac.joel.park@gmail.com>
2023-01-12 16:49:10 -0800

    add app.js

--- start/app.js
+++ start/app.js
@@ -7,7 +7,7 @@ const connectDB = require('./db/connect')
 const notFound = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error')
 
-const port = 3000;
+const port = process.env.PORT || 3000
 
 //middleware
 app.use(express.static('./public'))
