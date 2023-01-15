4f9b22ae4d40016a63c700fd225cad24d0fc8a0d
Isaac Park <isaac.joel.park@gmail.com>
2023-01-14 22:22:13 -0800

    add app.js

--- storeapi/app.js
+++ storeapi/app.js
@@ -6,6 +6,8 @@ require('dotenv').config()
 const express = require ('express')
 const app = express()
 
+const connectDB = require('./db/connect')
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
@@ -17,6 +19,7 @@ app.get('/', (req,res) => {
     res.send('<h1> Store API</h1> <a href = "/api/v1/products"')
 })
 
+app.use('/api/v1/products', )
 //products route
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
@@ -25,6 +28,7 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
     try{
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log("server is listenin port "))
 
     }catch(error){
---
+++ storeapi/controllers/products.js
@@ -0,0 +1,6 @@
+
+
+
+const getAllProductsStatic = asynce (req, res) =>{
+    res.status(200).json({msg:'products testing route'})
+}
