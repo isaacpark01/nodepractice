489b964806e4424e3bfcefec36b0ad53a724854e
Isaac Park <isaac.joel.park@gmail.com>
2023-01-14 22:36:28 -0800

    add app.js

--- storeapi/app.js
+++ storeapi/app.js
@@ -7,7 +7,7 @@ const express = require ('express')
 const app = express()
 
 const connectDB = require('./db/connect')
-
+const productsRouter = require('./routes/products')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
@@ -19,7 +19,7 @@ app.get('/', (req,res) => {
     res.send('<h1> Store API</h1> <a href = "/api/v1/products"')
 })
 
-app.use('/api/v1/products', )
+app.use('/api/v1/products', productsRouter)
 //products route
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
--- storeapi/controllers/products.js
+++ storeapi/controllers/products.js
@@ -1,6 +1,14 @@
 
 
 
-const getAllProductsStatic = asynce (req, res) =>{
+const getAllProductsStatic = async (req, res) =>  {
     res.status(200).json({msg:'products testing route'})
+}
+const getAllProducts = async (req, res) => {
+    res.status(200).json({msg: 'products route'})
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic,
 }
