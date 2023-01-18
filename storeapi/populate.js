823b3f202167735ceb17f66d55d8b7374f0dee6f
Isaac Park <isaac.joel.park@gmail.com>
2023-01-18 14:24:25 -0800

    add app.js

--- storeapi/populate.js
+++ storeapi/populate.js
@@ -9,7 +9,9 @@ const jsonProducts = require('./products.json')
 
 const start = async () =>{
     try{
-        await connectDB(processenv.MONGO_URI)
+        await connectDB(process.env.MONGO_URI)
+        await Product.deleteMany()
+        await Product.create(jsonProducts)
         console.log('Success!!!')
     } catch (error){
         console.log(error)
