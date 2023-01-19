5ab17d29eeb573fb2e97f02538839698bbf756ce
Isaac Park <isaac.joel.park@gmail.com>
2023-01-19 14:49:17 -0800

    add app.js

--- storeapi/controllers/products.js
+++ storeapi/controllers/products.js
@@ -4,20 +4,24 @@ const Product = require('../models/product')
 
 
 const getAllProductsStatic = async (req, res) =>  {
+    const search = 'a'
     const products = await Product.find({
         featured : true,
-        name: "vase table"
+        name: {$regex:search, $options: 'i'}, 
     })
     res.status(200).json({products, nbHits: products.length})
 }
 const getAllProducts = async (req, res) => {
-    const {featured, company} = req.query
-    const qureyObject =  {}
+    const {featured, company, name} = req.query
+    const queryObject =  {}
     if (featured){
         queryObject.featured = featured === 'true' ? true : false 
     }
     if (company){
         queryObject.company = company 
+    }
+    if(name){
+        queryObject.name = {$regex:name, $options: 'i'} 
     }
     console.log(queryObject) 
     const products = await Product.find(req.query)
