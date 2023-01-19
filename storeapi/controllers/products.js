33928034bf0740921891e681cba97d375d7f16ea
Isaac Park <isaac.joel.park@gmail.com>
2023-01-19 13:46:16 -0800

    add app.js

--- storeapi/controllers/products.js
+++ storeapi/controllers/products.js
@@ -1,4 +1,5 @@
 
+const { query } = require('express')
 const Product = require('../models/product')
 
 
@@ -10,7 +11,18 @@ const getAllProductsStatic = async (req, res) =>  {
     res.status(200).json({products, nbHits: products.length})
 }
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products route'})
+    const {featured, company} = req.query
+    const qureyObject =  {}
+    if (featured){
+        queryObject.featured = featured === 'true' ? true : false 
+    }
+    if (company){
+        queryObject.company = company 
+    }
+    console.log(queryObject) 
+    const products = await Product.find(req.query)
+    console.log(req.query);
+    res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports = {
