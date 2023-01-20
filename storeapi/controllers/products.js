5eac9f7178df473721c2853a2d2267eba7fc88fc
Isaac Park <isaac.joel.park@gmail.com>
2023-01-19 20:50:13 -0800

    add app.js

--- storeapi/controllers/products.js
+++ storeapi/controllers/products.js
@@ -4,11 +4,13 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) =>  {
     const search = 'a'
-    const products = await Product.find({}).sort('name price')
+    const products = await Product.find({})
+    .sort('name')
+    .select('name price')
     res.status(200).json({products, nbHits: products.length})
 }
 const getAllProducts = async (req, res) => {
-    const {featured, company, name, sort} = req.query
+    const {featured, company, name, sort, fields} = req.query
     const queryObject =  {}
     if (featured){
         queryObject.featured = featured === 'true' ? true : false 
@@ -28,6 +30,15 @@ const getAllProducts = async (req, res) => {
     else{
         result = result.sort('createdAt')
     }
+    if (fields){
+        const fieldList = sort.split(',').join(' ')
+        result = result.sort(fieldList)
+    }
+    const page = Number(req.query.page) || 1 
+    const limit = Number(req.query.limit) || 10
+    const skip = (page -1 ) * limit
+    result = result.skip(skip).limit(limit)
+
     const products = await result
     res.status(200).json({products, nbHits: products.length})
 }
