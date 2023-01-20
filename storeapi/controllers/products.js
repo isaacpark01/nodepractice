d66826725f3854943c6cf565732536ef3422d24e
Isaac Park <isaac.joel.park@gmail.com>
2023-01-19 21:00:16 -0800

    add app.js

--- storeapi/controllers/products.js
+++ storeapi/controllers/products.js
@@ -4,13 +4,13 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) =>  {
     const search = 'a'
-    const products = await Product.find({})
+    const products = await Product.find({price:{$gt:30}})
     .sort('name')
     .select('name price')
     res.status(200).json({products, nbHits: products.length})
 }
 const getAllProducts = async (req, res) => {
-    const {featured, company, name, sort, fields} = req.query
+    const {featured, company, name, sort, fields, numericFilters} = req.query
     const queryObject =  {}
     if (featured){
         queryObject.featured = featured === 'true' ? true : false 
@@ -20,6 +20,25 @@ const getAllProducts = async (req, res) => {
     }
     if(name){
         queryObject.name = { $regex:name, $options: 'i' } 
+    }
+    if (numericFilters){
+        const operatorMap = {
+            '>':'$gt',
+            '>=':'$gte',
+            '=':'$er',
+            '<':'$lt',
+            '<=':'$lte'
+        }
+        const regEx = /\b<|>|>=|=|<|<=\b/g
+        let filters = numbericFilters.replace(regEx, (match) => '-${operatorMap[match]}-')
+        const options = ['price', 'rating']
+        filters = filters.split('.').forEach((item)=>{
+            const [field, operator, value]= item.split('-')
+            if (options.includes(field)){
+                queryObject[field] = {[operator] : Number(value)}
+            }
+        })
+        console.log(filters)
     }
     console.log(queryObject) 
     let result = Product.find(queryObject)
