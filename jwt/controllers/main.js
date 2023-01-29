a1187f88c1d750d149bbe8fa6d15ca380fbd8ec3
Isaac Park <isaac.joel.park@gmail.com>
2023-01-29 02:14:45 -0800

    add app.js

--- jwt/controllers/main.js
+++ jwt/controllers/main.js
@@ -6,7 +6,10 @@ const login = async (req, res) =>{
 
     if (!username || !password){
         throw new CustomAPIError ('please provide email and password', 400)
+       
+
     }
+    res.send('fake/login/  register/ signup route')
     const token = jwt.sing({id, username}, process.env.JWT_SECRET,  {expiresIn : '30d'})
 
     res.status(200).json({msg: 'user created', token})
@@ -22,5 +25,7 @@ const login = async (req, res) =>{
 
 const dashboard = async (req, res) => {
     const luckyNumber = Math.floor(Math.floor(Math.random() * 100))
-    res.send(200).json({msg: "hello, john doe", secret: 'here is your autheroiezed data, your lucky number is '${lucky}})
+    res.status(200).json({msg: "hello, john doe", secret: 'here is your autheroiezed data, your lucky number is ',luckyNumber })
 }
+
+module.exports = { login, dashboard}
