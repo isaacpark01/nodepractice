636b749a5e37dd308fd8b2f6871593d01235cf43
Isaac Park <isaac.joel.park@gmail.com>
2023-02-02 17:43:49 -0800

    add app.js

--- jwt/errors/bad-request.js
+++ jwt/errors/bad-request.js
@@ -1,10 +1,10 @@
 const CustomAPIError = require('./custom-error')
-class CustomAPIError extends Error {
-    constructor(message, statusCode) {
+class BadRequest  extends CustomAPIError {
+    constructor(message) {
       super(message)
       this.statusCode = 400 
     }
   }
   
-  module.exports = CustomAPIError
+  module.exports = BadRequest
   
