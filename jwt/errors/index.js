006c239a44fa2b57efec4ff7ccb9a29ae7dbcf97
Isaac Park <isaac.joel.park@gmail.com>
2023-02-02 17:46:10 -0800

    add app.js

---
+++ jwt/errors/index.js
@@ -0,0 +1,8 @@
+const CustomAPIError = require('./custom-error')
+const BadRequestError = require('./custom-error')
+const UnauthenticatedError = require('./custom-error')
+
+module.exports = {
+    CustomAPIError, BadRequestError,
+    UnauthenticatedError
+}
