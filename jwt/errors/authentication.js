d4649db1d8926ee5a1c5be471e52061edc1359bc
Isaac Park <isaac.joel.park@gmail.com>
2023-02-02 17:49:29 -0800

    add app.js

--- jwt/errors/authentication.js
+++ jwt/errors/authentication.js
@@ -1,8 +1,9 @@
 const CustomAPIError = require('./custom-error')
+const {StatusCodes} = require('http-status-codes')
 class  UnauthenticatedError  extends CustomAPIError {
     constructor(message) {
       super(message)
-      this.statusCode = 401
+      this.statusCode = StatusCodes.UNAUTHORIZED
     }
   }
   
