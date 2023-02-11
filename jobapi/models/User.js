9dedce27b9b4eb859458af0d5b7835f56ef26113
Isaac Park <isaac.joel.park@gmail.com>
2023-02-10 17:46:53 -0800

    add app.js

---
+++ jobapi/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const UserSChema = new mongoose.Schema({
+    name:{
+        type:String, 
+        required:[true, "please prove a true name"], 
+        minlength: 3, 
+        maxlength: 50, 
+    }
+},
+    email:{
+        type:String, 
+        required:[true, "please prove a a email  "], 
+        match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 
+        "please provide valid email"],
+        unique: true, 
+
+}, 
+password:{
+    type:String, 
+    required : [true, "please enter a password"],
+    minlength: 6, 
+    maxlength: 12, 
+}
+)
+
+module.exports = mongoose.model('User', UserSchema)
