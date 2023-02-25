69d9111958b3bdf3ea2e8941f09323633c29fb96
Isaac Park <isaac.joel.park@gmail.com>
2023-02-25 00:15:23 -0800

    add app.js

--- storeapi/controllers/products.js
+++ storeapi/controllers/products.js
@@ -56,7 +56,7 @@ const getAllProducts = async (req, res) => {
     const page = Number(req.query.page) || 1 
     const limit = Number(req.query.limit) || 10
     const skip = (page -1 ) * limit
-    result = result.skip(skip).limit(limit)ddfe
+    result = result.skip(skip).limit(limit)
     const products = await result
     res.status(200).json({products, nbHits: products.length})
 }
