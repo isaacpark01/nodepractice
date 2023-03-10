1669b7cfd478a7bfdffced2c9315fc5e9bee7289
Isaac Park <isaac.joel.park@gmail.com>
2023-03-10 11:07:40 -0800

    add app.js

--- storeapi/app.js
+++ storeapi/app.js
@@ -21,7 +21,7 @@ app.get('/', (req,res) => {
 
 app.use('/api/v1/products', productsRouter)
 //products route
-app.use(notFounefceMiddleware)
+app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
 const port = process.env.PORT || 3000
