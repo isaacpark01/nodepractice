fcfc1d458f01568c15e42c1e2cd6c73261ce85d7
Isaac Park <isaac.joel.park@gmail.com>
2023-03-29 16:10:32 -0700

    add chap4.py

---
+++ python/imprac/chap4.py
@@ -0,0 +1,36 @@
+ciphertext = "16 12 8 4 0 1 5 9 13 17 18 14 10 6 2 3 7 11 15 19"
+
+cipherlist = list(ciphertext.split())
+
+COLS = 4 
+ROWS = 5 
+key = '-1 2 -3 4'
+
+translation_matrix = [None] * COLS
+
+plainttext = ''
+
+start = 0 
+stop = ROWS
+
+key_int = [int(i) for i in key.split()]
+
+for k in key_int:
+    if k < 0; 
+        col_items = cipherlist[start:stop]
+    elif k> 0:
+        col_items = list((reversed(cipherlist[start:stop])))
+    translation_matrix[abs(k) -1 ] = col_items
+    start += ROWS
+    stop += ROWS
+
+print("\nciphertext = {}".format(ciphertext))
+print("\ntranslation matrix =", *translation_matrix, sep= "\n")
+print("\nkey length = {}".format(len(key_int)))
+
+for i in range(ROWS):
+    for col_item in translation_matrix:
+        word = str(col_items.pop())
+        plainttext += word + ' '
+
+print("\nplaintext = {}".format(plainttext))
