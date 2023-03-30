974fa900a790c628be47cd74bee1b17592efb279
Isaac Park <isaac.joel.park@gmail.com>
2023-03-30 12:33:07 -0700

    add app.js

--- myown/middleware/auth.js
+++ myown/middleware/auth.js
@@ -1,21 +1,23 @@
 const jwt = require('jsonwebtoken')
-const {UnauthenticatedError} = require('../error')
-const authenticationMiddleware = async (req, res, next) =>{
+const {UnauthenticatedError } = require('../errors')
+const authenticationMiddleware  = async (req, res, next) =>{
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Bearer')){
-        throw new UnauthenticatedError('No token provided')
+        throw new UnauthenticatedError('No token provided ')
     }
     const token = authHeader.split(' ')[1]
+
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decoded = jwt.verify (token, process.env.JWT_SECRET)
         const {id, username} = decoded
-        req.user = {id, username}
+        req.user = {id,username}
         next()
-        const luckyNumber = Math.floot(Math.floor(Math.random() * 100))
-        res.status(200).json({msg: "hello, smoke ", secret: 'here is your authorized data, your lucky number is ', luckyNumber})
-    
-    } catch(error){
-        throw new UnauthenticatedError('Not authorized to access this route ') 
+        const luckyNumber = Math.floor(Math.floor(Math.random() * 100))
+        res.status(200).json({msg: "hello, john doe", secret: 'here is your autheroiezed data, your lucky number is ',luckyNumber })
+    } catch (error){
+        throw new UnauthenticatedError('Not authorized to acces this route ')
     }
+    
 }
+
 module.exports = authenticationMiddleware
