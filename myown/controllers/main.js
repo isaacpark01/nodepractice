8980e90857c44f5d7874e80cd55795771679724f
Isaac Park <isaac.joel.park@gmail.com>
2023-03-30 12:30:59 -0700

    add app.js

--- myown/controllers/main.js
+++ myown/controllers/main.js
@@ -2,22 +2,33 @@ const jwt = require('jsonwebtoken')
 const {BadRequestError} = require('../errors')
 
 const login = async (req, res) =>{
-    const{username, password} = req.body
+    const {username, password} = req.body
     console.log(username, password)
-    
-    if (!username || !password ){
+
+    if (!username || !password){
         throw new BadRequestError('please provide email and password')
     }
+
     const id = new Date().getDate()
-
+    
     const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn : '30d'})
+    
+    res.status(200).json({msg: 'user created', token})
 
-    res.status(200).json({msg: 'user created', token })
 }
 
-const dashboard = async(req, res ) => {
-    const luckyNumber = Math.floor(Math.floor(Math.random()  * 100))
-    res.status(200).json({msg: "hello, john doe", secret: 'here is your authorized data, your lucky number is ', luckyNumber})
-}
+// mongo
+//joi
+//check in controller
 
-module.exports = {login, dashboard}
+
+
+
+const dashboard = async (req, res) => {
+
+
+    const luckyNumber = Math.floor(Math.floor(Math.random() * 100))
+    res.status(200).json({msg: "hello, john doe", secret: 'here is your autheroiezed data, your lucky number is ',luckyNumber })
+   
+}
+module.exports = { login, dashboard}
