9a203bfa94fc0d063e794f57ef80f218c5249639
Isaac Park <isaac.joel.park@gmail.com>
2023-04-27 22:47:32 -0700

    add app.js

--- start/errors/custom-error.js
+++ start/errors/custom-error.js
@@ -14,4 +14,4 @@ const createCustomError = (msg, statusCode) => {
 }
 
 module.exports = {createCustomError, CustomAPIError}
-rv;iouhaervopunerawvpiouhaervpouhaerovuhbawepiuovrbaer;ouil
+rv;iouhaervopunerawvpiouhaervpouhaerovuhbawepiuovr
