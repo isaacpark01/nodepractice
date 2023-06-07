b813e2e56089cfb3dd50be7fbe0f5615d5b2009f
Isaac Park <isaac.joel.park@gmail.com>
2023-06-06 22:36:55 -0700

    add day 0110.py

---
+++ python/imprac4/day 01-10.py
@@ -0,0 +1,16 @@
+print('hellow python world')
+name = "ada lovelac"
+print(name.title())
+message = "one of python's strengths is its diverse community."
+print(message)
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
+bicycles.reverse()
+print(bicycles)
+squares = []
+for value in range(1,11):
+    square = value**2 
+    squares.append(square)
+
+
+print(squares)
