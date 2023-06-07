32dd03e39672739b669a5808c8ad3bb424d986fd
Isaac Park <isaac.joel.park@gmail.com>
2023-06-06 23:40:02 -0700

    add shoot.py

---
+++ python/imprac4/ship.py
@@ -0,0 +1,15 @@
+import pygame 
+def __init__(self, screen):
+    """initialize the ship and set its starting position."""
+    self.screen = screen 
+    # load the ship image and get its rect. 
+    self.image = pygame.image.load('images/ship.bmp')
+    self.rect = self.image.get_rect()
+    #start each new ship at the botoom center of the screen. 
+    self.rect.centerx = self.screen_rect.centerx
+    self.rect.bottom = self. screen_rect.bottom
+
+def blitme(self):
+    """draw the ship at its current location"""
+    self.screen.blit(self.image, self.rect)
+    
--- python/imprac4/shoot.py
+++ python/imprac4/shoot.py
@@ -1,7 +1,14 @@
 import sys 
 import pygame 
+
+from settings import Settings 
+
 def run_game():
     pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((
+        ai_settings.screen_width, ai_settings.screen_height
+    ))
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("alien invasion")
     bg_color = (230, 230, 230) 
@@ -13,7 +20,7 @@ def run_game():
                 sys.exit()
         # Redraw the screen during each pass through the loop. 
         screen.fill(bg_color)
-        
+        screen.fill(ai_settings.bg_color)
         pygame.display.flip()
 
 run_game()
